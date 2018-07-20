c110dc8b14c98f541d2336cb1256facf08165869
Reham Mostafa <RehamMostafa@BI.LOCAL>
2018-07-21 00:06:11 +0200

    First commit

--- src/app/reg/reg.component.ts
+++ src/app/reg/reg.component.ts
@@ -1,26 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import { NgModule } from '@angular/core';
-
 
 @Component({
   selector: 'reg',
   templateUrl: './reg.component.html',
   styleUrls: ['./reg.component.css']
 })
-@NgModule({
-  declarations: [
-    RegComponent
-  ],
-  imports: [
-    MatFormFieldModule
-  ]
-})
+
 export class RegComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
-  }
 
+}
 }
