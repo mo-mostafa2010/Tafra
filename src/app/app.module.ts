c3b19d2e81705a5b7848c43124c84aad44dfb94c
Reham Mostafa <RehamMostafa@BI.LOCAL>
2018-07-20 14:19:45 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import{MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule} from '@angular/material'
+import{FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
-
+import { RegComponent } from './reg/reg.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class PizzaPartyAppModule { }
